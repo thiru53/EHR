6114874f7679d833bf4fb82d56fa4ff3d2608aea
Tiru <thiru53@gmail.com>
2023-06-28 17:25:45 +0530

    task-3

--- src/main/java/com/webApp/controller/RegistrationController.java
+++ src/main/java/com/webApp/controller/RegistrationController.java
@@ -3,6 +3,7 @@ package com.webApp.controller;
 import com.webApp.entity.Patient;
 import com.webApp.repo.PatientRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,8 +24,12 @@ public class RegistrationController {
     }
 
     @PostMapping
-    public String registerPatient(Patient patient, BindingResult bindingResult) {
-        System.out.print("AAAA");
+    public String registerPatient(Model model, Patient patient, BindingResult bindingResult) {
+
+        if(patientRepository.existsByContactDetails(patient.getContactDetails())){
+            model.addAttribute("errMsg", "Contact details already registered");
+            return "registration-form";
+        }
         patientRepository.save(patient);
         return "redirect:/patients";
     }
