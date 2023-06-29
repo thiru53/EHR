f7eb5d50456a217c9f937e6fe9a2b451ad6021de
Tiru <thiru53@gmail.com>
2023-06-29 09:19:09 +0530

    task-3

--- src/main/java/com/webApp/controller/AppointmentController.java
+++ src/main/java/com/webApp/controller/AppointmentController.java
@@ -1,8 +1,10 @@
 package com.webApp.controller;
 
+import com.webApp.entity.Appointment;
 import com.webApp.service.AppointmentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +25,7 @@ public class AppointmentController {
     }
 
     @PostMapping("/schedule")
-    public String processAppointmentForm() {
+    public String processAppointmentForm(Appointment appointmentDetails, BindingResult bindingResult) {
         return "redirect:/patients";
     }
 
