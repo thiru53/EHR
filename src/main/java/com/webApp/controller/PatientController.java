9a582f80dc50b84a8d62799afe8f1ab982f711f1
Tiru <thiru53@gmail.com>
2023-06-29 09:19:09 +0530

    task-3

--- src/main/java/com/webApp/controller/PatientController.java
+++ src/main/java/com/webApp/controller/PatientController.java
@@ -7,12 +7,12 @@ import com.webApp.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/patients")
--- src/main/java/com/webApp/service/PatientService.java
+++ src/main/java/com/webApp/service/PatientService.java
@@ -2,13 +2,11 @@ package com.webApp.service;
 
 import com.webApp.entity.Patient;
 import com.webApp.repo.PatientRepository;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 @Service
 public class PatientService {
